25b447f0d86ecaf836251c96e2473cfd9cda0a14
Chris Icide <chris.icide@creditkarma.com>
2013-11-06 13:57:04 -0800

    try something else

--- proc2statsd/trending.py
+++ proc2statsd/trending.py
@@ -15,23 +15,33 @@ log.debug(slist)
 mode = utils.config.get("general", "mode")
 
 statsd_conn = raw = []
-for server in slist:
-    log.debug(server)
-    svr, prt = server.split(':')
-    sdc = statsd.Connection(host=svr, port=prt, sample_rate=1, disabled=False)
-    statsd_conn.append(sdc)
-    raw.append(statsd.Raw('procstats', sdc))
+
+statsDServerIP = '10.100.200.81'
+statsDServerPort = 8125
+statsd_conn = statsd.Connection(host=statsDServerIP, port=statsDServerPort, sample_rate=1, disabled=False)
+raw = statsd.Raw('dbManager', statsd_conn)
+
+def rawSend(name, value, tstamp):
+    #log.debug('trending %s, %s, %s' % (name, value, tstamp))
+    return raw.send(name, value, int(tstamp))
+
+#for server in slist:
+    #log.debug(server)
+    #svr, prt = server.split(':')
+    #sdc = statsd.Connection(host=svr, port=prt, sample_rate=1, disabled=False)
+    #statsd_conn.append(sdc)
+    #raw.append(statsd.Raw('procstats', sdc))
 
 
-def rawsend(name, value, tstamp):
-    if mode == "test":
-        log.debug('trending %s, %s, %s' % (name, value, tstamp))
-    else:
-        log.debug('trending %s, %s, %s' % (name, value, tstamp))
-        for r in raw:
-            log.debug(r)
-            #return r.send(name, value, tstamp)
-            return r.send(name, value)
+#def rawsend(name, value, tstamp):
+    #if mode == "test":
+        #log.debug('trending %s, %s, %s' % (name, value, tstamp))
+    #else:
+        #log.debug('trending %s, %s, %s' % (name, value, tstamp))
+        #for r in raw:
+            #log.debug(r)
+            ##return r.send(name, value, tstamp)
+            #return r.send(name, value)
 
 def collectandsend():
     log.debug('collect and send running')
