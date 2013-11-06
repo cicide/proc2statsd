f159c7f4de00bbbcee33fdcaadf1fc7159741a8b
Chris Icide <chris.icide@creditkarma.com>
2013-11-06 13:25:16 -0800

    add debug

--- proc2statsd/trending.py
+++ proc2statsd/trending.py
@@ -37,7 +37,7 @@ def collectandsend():
     procfile = utils.config.get("stats_diskstats", "file")
     regex = utils.config.get("stats_diskstats", "regex")
     regfield = int(utils.config.get("stats_diskstats", "regfield"))
-    statfields = utils.config.get("stats_diskstats", "statfields")
+    statfields = utils.config.get("stats_diskstats", "statfields").split(",")
     fieldnames = utils.config.get("stats_diskstats", "fieldnames")
     fieldtypes = utils.config.get("stats_diskstats", "fieldtypes")
 
