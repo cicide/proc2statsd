ff4fd7c0f92703bcdf428309c25ab2edf5f9a9b4
Chris Icide <chris.icide@creditkarma.com>
2013-11-06 13:30:14 -0800

    fix bad config file

--- proc2statsd/trending.py
+++ proc2statsd/trending.py
@@ -37,8 +37,8 @@ def collectandsend():
     procfile = utils.config.get("stats_diskstats", "file")
     regex = utils.config.get("stats_diskstats", "regex")
     regfield = int(utils.config.get("stats_diskstats", "regfield"))
-    statfields = utils.config.get("stats_diskstats", "statfields").split(",")
-    fieldnames = utils.config.get("stats_diskstats", "fieldnames").split(",")
+    statfields = utils.config.get("stats_diskstats", "statfields").split(",").strip()
+    fieldnames = utils.config.get("stats_diskstats", "fieldnames").split(",").strip()
     fieldtypes = utils.config.get("stats_diskstats", "fieldtypes")
 
     statsdata = procparse.getprocdata(procfile, regfield, regex, statfields)
